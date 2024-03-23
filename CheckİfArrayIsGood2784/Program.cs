5e3bb15ece7411857091ea7d240cdcc824643d8e
cagataycingiz <cagatayberk01@gmail.com>
2024-03-24 01:21:54 +0300

    2784 was hard. 2656

---
+++ CheckİfArrayIsGood2784/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Specialized;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsGood(new int[] { 5, 1, 2, 2, 5, 1 }));
+    }
+
+    /*
+     You are given an integer array nums. We consider an array good if it is a permutation of an array base[n].
+
+    base[n] = [1, 2, ..., n - 1, n, n] (in other words, it is an array of length n + 1 which contains 1 to n - 1 exactly once, plus two occurrences of n). For example, base[1] = [1, 1] and base[3] = [1, 2, 3, 3].
+
+    Return true if the given array is good, otherwise return false.
+
+    Note: A permutation of integers represents an arrangement of these numbers.
+     */
+    
+    public static bool IsGood(int[] nums)
+    {
+        Array.Sort(nums);
+        var n = nums.Length - 1;
+        for (int i = 0; i<n; i++)
+        {
+            if (nums[i] != i+1)
+                return false;
+        }
+        return nums[n]==n ? true : false;
+
+    }
+}
---
+++ MaximumSumWithExactlyKElements2656/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(MaximizeSum(new int[] {1,2,3,4,5},3));
+    }
+
+    /*
+     You are given a 0-indexed integer array nums and an integer k. Your task is to perform the following operation exactly k times in order to maximize your score:
+
+    Select an element m from nums.
+    Remove the selected element m from the array.
+    Add a new element with a value of m + 1 to the array.
+    Increase your score by m.
+    Return the maximum score you can achieve after performing the operation exactly k times.
+     */
+    public static int MaximizeSum(int[] nums, int k)
+    {
+        int point = 0;
+        Array.Sort(nums);
+
+        for(int i = 1;i<=k;i++)
+        {
+            int selected = nums[nums.Length-1] + 1;
+            nums[nums.Length-1] = selected;
+            point+=selected-1;
+        }
+        return point;
+    }
+
+}
