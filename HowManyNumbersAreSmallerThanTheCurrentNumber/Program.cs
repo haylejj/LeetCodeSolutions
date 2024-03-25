a16d9a9718f65246c34dc6d730e38470602b7484
cagataycingiz <cagatayberk01@gmail.com>
2024-03-26 00:47:40 +0300

    2114,1365

---
+++ HowManyNumbersAreSmallerThanTheCurrentNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var item in SmallerNumbersThanCurrent(new int[]{ 6,5,4,8}))
+        {
+            Console.Write(item+ " ");
+        }
+    }
+    /*
+     Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+     That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+     Return the answer in an array.
+     */
+    public static  int[] SmallerNumbersThanCurrent(int[] nums)
+    {
+        int count = 0;
+        int [] result=new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for(int j = 0; j<nums.Length; j++)
+            {
+                if (nums[i]>nums[j]) count++;
+            }
+            result[i] = count;
+            count=0;
+        }
+        return result;
+    }
+
+}
---
+++ MaximumNumberOfWordsFoundInSentences2114/Program.cs
@@ -0,0 +1,24 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(MostWordsFound(new string[] { "alice and bob love leetcode", "i think so too", "this is great thanks very much" }));
+    }
+    /*
+     A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+    You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+    Return the maximum number of words that appear in a single sentence.
+     */
+    public static int MostWordsFound(string[] sentences)
+    {
+        int max = 0;
+        foreach (string sentence in sentences)
+        {
+            int count = sentence.Trim().Split(' ').Count();
+            if (count > max) max = count;
+            count = 0;
+        }
+        return max;
+    }
+}
