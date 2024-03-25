bb4f414138e738fccc518a177d798c80b81d5d6b
cagataycingiz <cagatayberk01@gmail.com>
2024-03-26 00:16:50 +0300

    1480

---
+++ RunningSumOf1dArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Specialized;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var arg in RunningSum(new int[] { 3, 1, 2, 10, 1 }))
+        {
+            Console.Write(arg+" ");
+        }
+    }
+    /*
+     Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+     Return the running sum of nums.
+     */
+    public static int[] RunningSum(int[] nums)
+    {
+        int sum = 0;
+        int[] result=nums.Select(x => sum += x).ToArray();
+        return result;
+    }
+
+}
