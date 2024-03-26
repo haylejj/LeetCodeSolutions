c4643683ea56deb3bee6ac98912f60e1a4cd2b81
cagataycingiz <cagatayberk01@gmail.com>
2024-03-27 01:28:57 +0300

    2855,2859

---
+++ MinimumRightShiftsToSortTheArray2855/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine(MinimumRightShifts(new List<int> { 3, 4, 5, 1, 2 }));
+    }
+    /*
+     You are given a 0-indexed array nums of length n containing distinct positive integers. 
+    Return the minimum number of right shifts required to sort nums and -1 if this is not possible.
+    A right shift is defined as shifting the element at index i to index (i + 1) % n, for all indices.
+     */
+    public static int MinimumRightShifts(IList<int> nums)
+    {
+        int pivots = 0;
+        int n = nums.Count;
+        int ans = 0;
+        for (int i = 0; i != n; ++i)
+        {
+            if (nums[i] >= nums[(i - 1 + n) % n]) continue;
+            ++pivots;
+            ans = (n - i) % n; // the number of values after the pivot
+        }
+        if (pivots > 1) return -1; // too many pivots
+        return ans;
+
+
+
+
+
+        //int count = 0;
+
+        //for(int j=0; j<nums.Count; j++)
+        //{
+        //    int temp = nums[nums.Count - 1];
+        //    for (int i = nums.Count - 1; i > 0; i--)
+        //    {
+        //        nums[i] = nums[i - 1];
+        //    }
+        //    nums[0] = temp;
+        //    count++;
+        //}
+        //return count;
+    }
+    //public static bool isSort(IList<int> nums)
+    //{
+    //    var sortedList = nums.OrderBy(x => x).ToList();
+    //    return nums.SequenceEqual(sortedList);
+    //}
+}
---
+++ SumOfValuesAtIndicesWithKSetBits/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write(SumIndicesWithKSetBits(new int[] { 4, 3, 2, 1 },2));
+    }
+    /*
+     You are given a 0-indexed integer array nums and an integer k.
+    Return an integer that denotes the sum of elements in nums whose corresponding indices have exactly k set bits in their binary representation.
+    The set bits in an integer are the 1's present when it is written in binary.
+    For example, the binary representation of 21 is 10101, which has 3 set bits.
+     */
+    public static int SumIndicesWithKSetBits(IList<int> nums, int k)
+    {
+        int sum = 0;
+        for (int i = 0; i < nums.Count; i++)
+        {
+            if (BinaryControl(i,k))
+                sum += nums[i];
+        }
+        return sum;
+    }
+    public static bool BinaryControl(int num,int k)
+    {
+        //8/2=4 remain 0
+        int count = 0;
+        int remain = 0;
+        while(num > 0)
+        {
+            remain=num%2;
+            if(remain == 1) count++;
+            num=num/2;
+        }
+        return count  == k;
+    }
+}
