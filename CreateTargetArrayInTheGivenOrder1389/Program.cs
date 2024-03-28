d66877280af823b36671385c202f83bbed2de938
cagataycingiz <cagatayberk01@gmail.com>
2024-03-28 23:23:59 +0300

    1389,2574

---
+++ CreateTargetArrayInTheGivenOrder1389/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var item in CreateTargetArray(new int[] { 0,1,2,3,4,7 },new int[] {0,1,2,2,2,2}))
+        {
+            Console.Write(item+" ");
+        }
+    }
+    /*
+     Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+
+    Initially target array is empty.
+    From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+    Repeat the previous step until there are no elements to read in nums and index.
+    Return the target array.
+
+    It is guaranteed that the insertion operations will be valid.
+     */
+    public static int[] CreateTargetArray(int[] nums, int[] index)
+    {
+        //int[] targetArr= new int[nums.Length];
+
+        //for(int i = 0; i<nums.Length; i++)
+        //{
+        //    for(int j=nums.Length-1; j> index[i]; j--)
+        //    {
+        //        targetArr[j]=targetArr[j-1];
+        //    }
+        //    targetArr[index[i]]= nums[i];
+        //}
+        //return targetArr;
+        List<int> list = new List<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            list.Insert(index[i], nums[i]);
+        }
+        return list.ToArray();
+    }
+}
---
+++ LeftAndRightSumDifferences2574/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var item in LeftRightDifference(new int[] {10,4,8,3}))
+        {
+            Console.Write(item+" ");
+        }
+    }
+    /*
+     Given a 0-indexed integer array nums, find a 0-indexed integer array answer where:
+
+    answer.length == nums.length.
+    answer[i] = |leftSum[i] - rightSum[i]|.
+    Where:
+
+    leftSum[i] is the sum of elements to the left of the index i in the array nums. If there is no such element, leftSum[i] = 0.
+    rightSum[i] is the sum of elements to the right of the index i in the array nums. If there is no such element, rightSum[i] = 0.
+    Return the array answer.
+     */
+    public static int[] LeftRightDifference(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+        for(int i=0; i<nums.Length; i++)
+        {
+            result[i]=Math.Abs(nums.Take(i).Sum() - nums.Skip(i+1).Sum());
+        }
+        return result;
+    }
+}
