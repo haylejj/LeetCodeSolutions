fa8b67f6996026ead528b876c0376e264bec455c
cagataycingiz <cagatayberk01@gmail.com>
2024-03-29 00:47:24 +0300

    1773,1662

---
+++ CheckIfTwoStringArraysAreEquivalent1662/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write(ArrayStringsAreEqual(new string[] { "ab", "c" },new string[] {"a","bc"}));
+    }
+    /*
+    Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+
+    A string is represented by an array if the array elements concatenated in order forms the string.
+     */
+    public static bool ArrayStringsAreEqual(string[] word1, string[] word2)
+    {
+        return string.Join("", word1).SequenceEqual(string.Join("", word2));
+        
+    }
+}
---
+++ CountItemsMatchingARule1773/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+    /*
+    You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item.
+    You are also given a rule represented by two strings, ruleKey and ruleValue.
+    The ith item is said to match the rule if one of the following is true:
+    ruleKey == "type" and ruleValue == typei.
+    ruleKey == "color" and ruleValue == colori.
+    ruleKey == "name" and ruleValue == namei.
+    Return the number of items that match the given rule.
+     */
+    public static int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+    {
+        //type color name
+        int count = 0;
+        foreach (var item in items)
+        {
+            if (ruleKey=="type")
+            {
+                if (item[0] == ruleValue) count++;
+            }
+            else if (ruleKey=="color")
+            {
+                if (item[1]==ruleValue) count++;
+            }
+            else
+            {
+                if (item[2]==ruleValue) count++;
+            }
+        }
+        return count;
+    }
+}
