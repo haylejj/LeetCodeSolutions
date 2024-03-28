02639563064173535b3459e4299c7dfd750913ca
cagataycingiz <cagatayberk01@gmail.com>
2024-03-29 00:24:31 +0300

    1313,2974

---
+++ DecompressRunLengthEncodedList1313/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var item in DecompressRLElist(new int[] { 1, 1, 2, 3 }))
+        {
+            Console.Write(item+" ");
+        }
+    }
+    /*
+     We are given a list nums of integers representing a list compressed with run-length encoding.
+
+    Consider each adjacent pair of elements [freq, val] = [nums[2*i], nums[2*i+1]] (with i >= 0). 
+    For each such pair, there are freq elements with value val concatenated in a sublist. 
+    Concatenate all the sublists from left to right to generate the decompressed list.
+
+    Return the decompressed list.
+     */
+    public static int[] DecompressRLElist(int[] nums)
+    {
+        List<int> result = new List<int>();
+
+        int n=nums.Length/2;
+
+        for(int i = 0;i < n; i++)
+        {
+            for(int j=0;j < nums[2*i]; j++)
+            {
+                result.Add(nums[(2*i)+1]);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
---
+++ MinimumNumberGame2974/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var item in NumberGame(new int[] {5,4,2,3}))
+        {
+            Console.Write(item+" ");
+        }
+    }
+    /*
+    You are given a 0-indexed integer array nums of even length and there is also an empty array arr. 
+    Alice and Bob decided to play a game where in every round Alice and Bob will do one move. The rules of the game are as follows:
+    Every round, first Alice will remove the minimum element from nums, and then Bob does the same.
+    Now, first Bob will append the removed element in the array arr, and then Alice does the same.
+    The game continues until nums becomes empty.
+    Return the resulting array arr.
+     */
+    public static int[] NumberGame(int[] nums)
+    {
+        Array.Sort(nums);
+        int temp = 0;
+        for(int i=0;i<nums.Length;i+=2)
+        {
+            temp=nums[i];
+            nums[i]=nums[i+1];
+            nums[i+1]=temp;
+        }
+        return nums;
+    }
+}
