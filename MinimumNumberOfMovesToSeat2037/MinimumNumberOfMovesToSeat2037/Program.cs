05691977ed0d1cbef39565ad80951bd16fae15c2
cagataycingiz <cagatayberk01@gmail.com>
2024-06-20 19:56:22 +0300

    2037,1863 easy

---
+++ MinimumNumberOfMovesToSeat2037/MinimumNumberOfMovesToSeat2037/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(MinMovesToSeat(new int[] { 2, 2, 6, 6 },new int[] { 1, 3, 2, 6 }));
+    }
+    public static int MinMovesToSeat(int[] seats, int[] students)
+    {
+        int movesSum = 0;
+        Array.Sort(seats);
+        Array.Sort(students);
+        for(int i = 0; i<seats.Length; i++)
+        {
+           movesSum += Math.Abs(seats[i] - students[i]);
+        }
+        return movesSum;
+    }
+}
---
+++ SumOfAllSubsetXORTotal1863/SumOfAllSubsetXORTotal1863/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(SubsetXORSum(new int[] { 3, 4, 5, 6, 7, 8 }));
+    }
+    public static int SubsetXORSum(int[] nums)
+    {
+        return XorHelper(nums,0,0);
+    }
+    private static  int XorHelper(int[] nums,int index,int Xor)
+    {
+        if(index ==nums.Length)
+        {
+            return Xor;
+        }
+        return XorHelper(nums,index+1,Xor^nums[index])+XorHelper(nums,index+1,Xor);
+    }
+}
