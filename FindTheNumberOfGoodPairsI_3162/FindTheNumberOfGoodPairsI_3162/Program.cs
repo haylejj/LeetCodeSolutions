1c8e5a53cec68fa3bfe13f565cbe14fc84a58337
cagataycingiz <cagatayberk01@gmail.com>
2024-06-21 11:00:14 +0300

    3065,3162

---
+++ FindTheNumberOfGoodPairsI_3162/FindTheNumberOfGoodPairsI_3162/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(NumberOfPairs(new int[] {1,2,4,12},new int[] {2,4},3));
+    }
+    public static int NumberOfPairs(int[] nums1, int[] nums2, int k)
+    {
+        int n=nums1.Length;
+        int m=nums2.Length;
+        int count=0;
+        for(int i = 0; i<n; i++)
+        {
+            for(int j = 0; j<m; j++)
+            {
+                  if (nums1[i] % (nums2[j]*k)==0) count++;
+            }
+        }
+        return count;  
+    }
+}
+
---
+++ MinimumToExceedThresholdValue1_3065/MinimumToExceedThresholdValue1_3065/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    /*
+     * You are given a 0-indexed integer array nums, and an integer k.
+
+    In one operation, you can remove one occurrence of the smallest element of nums.
+
+    Return the minimum number of operations needed so that all elements of the array are greater than or equal to k.
+     */
+    static void Main(string[] args)
+    {
+        
+    }
+    public int MinOperations(int[] nums, int k) => nums.Count(x => x < k);
+    
+}
+
+
