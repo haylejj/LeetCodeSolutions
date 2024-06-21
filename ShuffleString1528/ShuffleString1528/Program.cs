84b3005bcbb34beb92718d74613ac3200223c8b5
cagataycingiz <cagatayberk01@gmail.com>
2024-06-21 12:54:04 +0300

    1528,1816 easy

---
+++ ShuffleString1528/ShuffleString1528/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+class Program
+{
+    /*
+     You are given a string s and an integer array indices of the same length. The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+    Return the shuffled string.
+     */
+    static void Main(string[] args)
+    {
+        Console.WriteLine(RestoreString("codeleet", new int[] { 4, 5, 6, 7, 0, 2, 1, 3 }));
+    }
+    public static string RestoreString(string s, int[] indices)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i<s.Length; i++)
+        {
+            for (int j = 0; j<indices.Length; j++)
+            {
+                if (indices[j] == i)
+                {
+                    stringBuilder.Append(s[j]);
+                }
+            }
+        }
+        return stringBuilder.ToString();
+    }
+}
---
+++ TruncateSentence1816/TruncateSentence1816/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+class Program
+{
+    /*
+     A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+
+    For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+    You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+
+ 
+     */
+    static void Main(string[] args)
+    {
+        Console.WriteLine(TruncateSentence("Hello World A B C D", 3));
+    }
+    public static string TruncateSentence(string s, int k)
+    {
+
+        string[] result = s.Split(' ');
+        return string.Join(" ", result,0,k);
+    }
+
+}
+
+
