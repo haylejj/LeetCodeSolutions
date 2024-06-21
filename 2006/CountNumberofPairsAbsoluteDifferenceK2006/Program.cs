3fb020ffb626f38f35a7b1792bf0bdaf6a298e5c
cagataycingiz <cagatayberk01@gmail.com>
2024-06-22 01:48:14 +0300

    2006,2367 easy

---
+++ 2006/CountNumberofPairsAbsoluteDifferenceK2006/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    /*
+    Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+
+    The value of |x| is defined as:
+
+    x if x >= 0.
+    -x if x < 0.
+ 
+     */
+    static void Main(string[] args)
+    {
+        Console.WriteLine(CountKDifference(new int[] { 3, 2, 1, 5, 4 },2));
+    }
+    public static int CountKDifference(int[] nums, int k)
+    {
+        int count = 0;
+        for(int i=0; i<nums.Length; i++)
+        {
+            for(int j=i+1; j<nums.Length; j++)
+            {
+                    if (Math.Abs(nums[i] - nums[j]) == k)
+                    {
+                        count++;
+                    }
+            }
+        }
+        return count;
+    }
+}
---
+++ NumberOfArithmeticTriplets2367/NumberOfArithmeticTriplets2367/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    /*
+    You are given a 0-indexed, strictly increasing integer array nums and a positive integer diff. A triplet (i, j, k) is an arithmetic triplet if the following conditions are met:
+
+    i < j < k,
+    nums[j] - nums[i] == diff, and
+    nums[k] - nums[j] == diff.
+    Return the number of unique arithmetic triplets.
+     */
+    static void Main(string[] args)
+    {
+        Console.WriteLine(ArithmeticTriplets(new int[] { 0, 1, 4, 6, 7, 10 },3));
+    }
+    public static int ArithmeticTriplets(int[] nums, int diff)
+    {
+        int count=0;
+        for(int i = 0; i<nums.Length-2; i++)
+        {
+            for(int j = i+1; j<nums.Length-1; j++)
+            {
+                if (nums[j]-nums[i]==diff)
+                {
+                    for(int k = j+1; k<nums.Length; k++)
+                    {
+                        if (nums[k]-nums[j]==diff) count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
