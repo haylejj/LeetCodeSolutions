8466bd9e00ef59b7ed50c8a693c8d5f91fa61a8a
cagataycingiz <cagatayberk01@gmail.com>
2024-06-23 13:14:29 +0300

    1572,2828,804 easy

---
+++ 2828/2828/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    /*
+      Given an array of strings words and a string s, determine if s is an acronym of words.
+
+      The string s is considered an acronym of words if it can be formed by concatenating the first character of each string in words in order. For example, "ab" can be formed from ["apple", "banana"], but it can't be formed from ["bear", "aardvark"].
+
+       Return true if s is an acronym of words, and false otherwise.
+     */
+    static void Main(string[] args)
+    {
+        //["ekin","f","rabj","driadwjqz"]
+        Console.WriteLine(IsAcronym(new List<string> { "ekin", "f", "rabj", "driadwjqz" }, "defr")); // false
+        Console.WriteLine(IsAcronym(new List<string> { "an", "apple" }, "a"));
+    }
+    public static bool IsAcronym(IList<string> words, string s)
+    {
+        for (int i = 0; i<s.Length; i++) 
+        {   
+            if(s.Length != words.Count) return false;
+
+            if (s[i] != words[i][0]) 
+            {
+                return false;
+            }
+            
+        }
+        return true;
+    }
+}
---
+++ 804/804/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+
+class Program
+{
+    /*
+     * International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+    'a' maps to ".-",
+    'b' maps to "-...",
+    'c' maps to "-.-.", and so on.
+    For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+    [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+    Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+    For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+    Return the number of different transformations among all words we have.
+     */
+    static void Main(string[] args)
+    {
+
+    }
+    public int UniqueMorseRepresentations(string[] words)
+    {
+        Dictionary<string, string> morseAlfabe = new Dictionary<string, string>()
+        {
+            {"a", ".-"},
+            {"b", "-..."},
+            {"c", "-.-."},
+            {"d", "-.."},
+            {"e", "."},
+            {"f", "..-."},
+            {"g", "--."},
+            {"h", "...."},
+            {"i", ".."},
+            {"j", ".---"},
+            {"k", "-.-"},
+            {"l", ".-.."},
+            {"m", "--"},
+            {"n", "-."},
+            {"o", "---"},
+            {"p", ".--."},
+            {"q", "--.-"},
+            {"r", ".-."},
+            {"s", "..."},
+            {"t", "-"},
+            {"u", "..-"},
+            {"v", "...-"},
+            {"w", ".--"},
+            {"x", "-..-"},
+            {"y", "-.--"},
+            {"z", "--.."},
+        };
+        HashSet<string> result = new HashSet<string>();
+        foreach (var item in words)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach(var letter in item)
+            {
+                stringBuilder.Append(morseAlfabe[letter.ToString()]);
+            }
+            result.Add(stringBuilder.ToString());
+        }
+        return result.Count;
+    }
+}
