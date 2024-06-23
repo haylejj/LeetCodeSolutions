36d6fcb6885a8ab8f4c9305c2451319b09685a49
cagataycingiz <cagatayberk01@gmail.com>
2024-06-23 11:53:09 +0300

    1732,2956 easy

---
+++ 1732/1732/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    /*
+     There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
+
+    You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+     */
+    static void Main(string[] args)
+    {
+        Console.WriteLine(LargestAltitude(new int[] { -5, 1, 5, 0, -7 }));
+    }
+    public static int LargestAltitude(int[] gain)
+    {
+        int[] altitude=new int[gain.Length + 1];
+        altitude[0] = 0;
+        for(int i = 0; i<altitude.Length-1; i++)
+        {
+            altitude[i+1]= altitude[i] + gain[i];
+        }
+        return altitude.Max();
+    }
+}
---
+++ 2956/2956/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    /*
+     You are given two integer arrays nums1 and nums2 of sizes n and m, respectively. Calculate the following values:
+
+    answer1 : the number of indices i such that nums1[i] exists in nums2.
+    answer2 : the number of indices i such that nums2[i] exists in nums1.
+    Return [answer1,answer2].
+     */
+    static void Main(string[] args)
+    {
+        foreach (var item in FindIntersectionValues(new int[] { 2, 3, 2 },new int[] {1,2}))
+        {
+            Console.Write(item);
+        }
+    }
+    public static int[] FindIntersectionValues(int[] nums1, int[] nums2)
+    {
+        int[] result=new int[2];
+        int count1 = 0;
+        int count2 = 0;
+        result[0] = nums1.Count(x => nums2.Contains(x));
+        result[1] = nums2.Count(x => nums1.Contains(x));
+
+        return result;
+    }
+}
