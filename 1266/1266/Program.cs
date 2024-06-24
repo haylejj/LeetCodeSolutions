186f5a87b56231a62b113987b19b508d82aa0ff6
cagataycingiz <cagatayberk01@gmail.com>
2024-06-24 13:02:44 +0300

    1266,3131,832 easy

---
+++ 1266/1266/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    /*
+     On a 2D plane, there are n points with integer coordinates points[i] = [xi, yi]. Return the minimum time in seconds to visit all the points in the order given by points.
+
+    You can move according to these rules:
+
+    In 1 second, you can either:
+    move vertically by one unit,
+    move horizontally by one unit, or
+    move diagonally sqrt(2) units (in other words, move one unit vertically then one unit horizontally in 1 second).
+    You have to visit the points in the same order as they appear in the array.
+    You are allowed to pass through points that appear later in the order, but these do not count as visits.
+     */
+    static void Main(string[] args)
+    {
+        Console.WriteLine(MinTimeToVisitAllPoints(new int[][] { new int[] { 1, 1 }, new int[] { 3, 4 }, new int[] { -1, 0 } }));
+    }
+    public static int MinTimeToVisitAllPoints(int[][] points)
+    {
+        int count = 0;
+        for (int i = 0; i<points.Length-1; i++) {
+            count+= Math.Max(Math.Abs(points[i][0] - points[i+1][0]), Math.Abs(points[i][1] - points[i+1][1]));
+        }
+        return count;
+    }
+}
---
+++ 3131/3131/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    /*
+     You are given two arrays of equal length, nums1 and nums2.
+
+    Each element in nums1 has been increased (or decreased in the case of negative) by an integer, represented by the variable x.
+
+    As a result, nums1 becomes equal to nums2. Two arrays are considered equal when they contain the same integers with the same frequencies.
+
+    Return the integer x.
+
+ 
+     */
+    static void Main(string[] args)
+    {
+
+    }
+    public int AddedInteger(int[] nums1, int[] nums2)
+    {
+        Array.Sort(nums1);
+        Array.Sort(nums2);
+        return nums2[0]- nums1[0];
+    }
+}
