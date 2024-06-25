5e7fb90906613e10dc370ba7e40451bba5bb1e95
cagataycingiz <cagatayberk01@gmail.com>
2024-06-25 13:50:23 +0300

    1827,2418,2960 easy

---
+++ 1827/1827/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    /*
+     You are given an integer array nums (0-indexed). In one operation, you can choose an element of the array and increment it by 1.
+
+    For example, if nums = [1,2,3], you can choose to increment nums[1] to make nums = [1,3,3].
+    Return the minimum number of operations needed to make nums strictly increasing.
+
+    An array nums is strictly increasing if nums[i] < nums[i+1] for all 0 <= i < nums.length - 1. An array of length 1 is trivially strictly increasing.
+
+ 
+     */
+    static void Main(string[] args)
+    {
+        Console.WriteLine(MinOperations(new int[] {1,5,2,4,1}));
+    }
+    public static int MinOperations(int[] nums)
+    {
+        //1,5,2,4,1
+        //1,1,2,4,5
+        int count = 0;
+        for (int i = 0;  i< nums.Length-1; i++)
+        {
+            if (nums[i] >= nums[i+1])
+            {
+                count+=nums[i]-nums[i+1]+1;
+                nums[i+1]=nums[i]+1;
+            }
+        }
+        return count;
+    }
+}
---
+++ 2418/2418/Program.cs
@@ -0,0 +1,47 @@
+﻿class Program
+{
+    /*
+     You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+
+    For each index i, names[i] and heights[i] denote the name and height of the ith person.
+
+    Return names sorted in descending order by the people's heights.
+     */
+    static void Main(string[] args)
+    {
+        Console.WriteLine(SortPeople(new string[] { "Mary", "John", "Emma" },new int[] { 180, 165, 170 }));
+    }
+    public static string[] SortPeople(string[] names, int[] heights)
+    {
+        //["Mary","John","Emma"]
+        //[180,165,170]
+        //mary 180
+        //john 165
+        //emma 170
+
+        Dictionary<int, string> dict = new Dictionary<int, string>();
+        for (int i = 0; i < names.Length; i++)
+        {
+            dict.Add(heights[i], names[i]);
+        }
+        //Bubble sort
+        for(int i = 0; i<heights.Length-1; i++)
+        {
+            for(int j = 0; j<heights.Length-1-i; j++)
+            {
+                if (heights[j]<heights[j+1])
+                {
+                    int temp=heights[j];
+                    heights[j]=heights[j+1];
+                    heights[j+1]=temp;
+                }
+            }
+        }
+        for(int i = 0; i<heights.Length; i++)
+        {
+            names[i]=dict[heights[i]];
+        }
+        return names;
+
+    }
+}
