d1d2032dd834e7f55332b234b9cc13486e0044fd
cagataycingiz <cagatayberk01@gmail.com>
2024-06-27 01:18:46 +0300

    561,2553 easy

---
+++ 2553/2553/Program.cs
@@ -0,0 +1,42 @@
+﻿
+class Program
+{
+    /*
+    Given an array of positive integers nums, return an array answer that consists of the digits of each integer in nums 
+     
+    after separating them in the same order they appear in nums.
+
+    To separate the digits of an integer is to get all the digits it has in the same order.
+
+    For example, for the integer 10921, the separation of its digits is [1,0,9,2,1].
+ 
+     */
+    static void Main(string[] args)
+    {
+
+    }
+    public int[] SeparateDigits(int[] nums)
+    {
+        List<int> list = new List<int>();
+        List<int> list2 = new List<int>();
+        int count = 0;
+
+        for (int i = 0; i<nums.Length; i++) 
+        {
+            if (nums[i]<10) list.Add(nums[i]);
+            //13 250/10
+            else
+            {
+                while (nums[i]>0)
+                {
+                    list.Add(nums[i] % 10);
+                    nums[i] /= 10;
+                }
+            }
+            list.Reverse();
+            list2.AddRange(list);
+            list.Clear();
+        }
+        return list2.ToArray();
+    }
+}
---
+++ 561/561/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    /*
+     Given an integer array nums of 2n integers, 
+    group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. 
+    Return the maximized sum.
+     */
+    static void Main(string[] args)
+    {
+
+    }
+    public int ArrayPairSum(int[] nums)
+    {
+        Array.Sort(nums);
+        int sum = 0;
+        sum=Math.Min(nums[nums.Length - 1], nums[nums.Length - 2]);
+        for (int i = 0; i < nums.Length - 2; i += 2)
+        {
+            sum += Math.Min(nums[i], nums[i + 1]);
+        }
+        return sum;
+    }
+}
