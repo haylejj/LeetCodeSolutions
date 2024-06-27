e4cc99d56f5e2a19be514dd87dc2312a843a2a34
cagataycingiz <cagatayberk01@gmail.com>
2024-06-27 16:57:01 +0300

    1252,1725

---
+++ 1252/1252/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+    /*
+     There is an m x n matrix that is initialized to all 0's. There is also a 2D array indices where each indices[i] = [ri, ci] represents a 0-indexed location to perform some increment operations on the matrix.
+
+    For each location indices[i], do both of the following:
+
+    Increment all the cells on row ri.
+    Increment all the cells on column ci.
+    Given m, n, and indices, return the number of odd-valued cells in the matrix after applying the increment to all locations in indices.
+     */
+    static void Main(string[] args)
+    {
+
+    }
+    public int OddCells(int m, int n, int[][] indices)
+    {
+        //indices = [[0,1],[1,1]]
+        int[,] result=new int[m,n];
+        for(int i = 0; i<indices.Length; i++)
+        {
+            for(int j = 0; j<n; j++)
+            {
+                result[indices[i][0], j]++;
+            }
+            for(int k = 0; k<m; k++)
+            {
+                result[k, indices[i][1]]++;
+            }
+        }
+        int count = 0;
+        for(int i = 0; i<m; i++)
+        {
+            for(int j = 0; j<n; j++)
+            {
+                if(result[i,j]%2!=0)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
---
+++ 1725/1725/Program.cs
@@ -0,0 +1,50 @@
+﻿class Program
+{
+    /*
+     You are given an array rectangles where rectangles[i] = [li, wi] represents the ith rectangle of length li and width wi.
+
+    You can cut the ith rectangle to form a square with a side length of k if both k <= li and k <= wi. For example, if you have a rectangle [4,6], you can cut it to get a square with a side length of at most 4.
+
+    Let maxLen be the side length of the largest square you can obtain from any of the given rectangles.
+
+    Return the number of rectangles that can make a square with a side length of maxLen.
+     */
+    static void Main(string[] args)
+    {
+        int[][] rectangles = new int[][] { new int[] { 5, 8 }, new int[] { 3, 9 }, new int[] { 5, 12 }, new int[] { 16, 5 } };
+        Console.WriteLine(CountGoodRectangles(rectangles));
+    }
+    public static int CountGoodRectangles(int[][] rectangles)
+    {
+        List<int> list = new List<int>();
+        foreach (var item in rectangles)
+        {
+            if (item[0]>=item[1])
+            {
+                list.Add(item[1]);
+            }
+            else
+            {
+                list.Add(item[0]);
+            }
+        }
+        //int maxLength = 0;
+        //int count = 0;
+        //foreach (var item in list)
+        //{
+        //    if (maxLength==Math.Pow(item, 2)) count++;
+        //    var temp=(int)Math.Pow(item, 2);
+        //    if (temp>maxLength) { maxLength = temp; count = 1;}
+        //}
+        int count = 0;
+        int maxLength = list.Max();
+        foreach (var item in list)
+        {
+            if (item==maxLength)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
