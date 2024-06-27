acd8655aa235d727bc52edeba5a9f131fcaaa826
cagataycingiz <cagatayberk01@gmail.com>
2024-06-27 15:08:08 +0300

    2215,3005 easy

---
+++ 2215/2215/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    /*
+     Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+    answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+    answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+    Note that the integers in the lists may be returned in any order.
+     */
+    static void Main(string[] args)
+    {
+
+    }
+    public IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+    {
+        HashSet<int> set1 = new HashSet<int>(nums1);
+        HashSet<int> set2 = new HashSet<int>(nums2);
+        List<int> result1 = new List<int>();
+        List<int> result2 = new List<int>();
+        foreach (int num in set1)
+        {
+            if (!set2.Contains(num))
+            {
+                result1.Add(num);
+            }
+        }
+
+        foreach (int num in set2)
+        {
+            if (!set1.Contains(num))
+            {
+                result2.Add(num);
+            }
+        }
+        IList<IList<int>> result = new List<IList<int>>();
+        result.Add(result1);
+        result.Add(result2);
+        return result;
+    }
+}
---
+++ 3005/3005/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    /*
+     You are given an array nums consisting of positive integers.
+
+    Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+
+    The frequency of an element is the number of occurrences of that element in the array.
+
+ 
+     */
+    static void Main(string[] args)
+    {
+        int[] nums = { 10, 12, 11, 9, 6, 19, 11 };
+        Console.WriteLine(MaxFrequencyElements(nums));
+    }
+    public static int MaxFrequencyElements(int[] nums)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        for(int i = 0; i<nums.Length; i++)
+        {
+            if (dict.ContainsKey(nums[i]))
+            {
+                dict[nums[i]]++;
+            }
+            else
+            {
+                dict.Add(nums[i], 1);
+            }
+        }
+        int maximumFrequency = dict.Values.Max();
+        return dict.Where(x=>x.Value==maximumFrequency).Sum(x=>x.Value);
+    }
+}
