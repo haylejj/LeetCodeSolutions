2b0b5fcb6b9cd168ad83a4c77ba3d000ae9c00ca
cagataycingiz <cagatayberk01@gmail.com>
2024-07-02 02:17:03 +0300

    1304,3151 easy

---
+++ 1304/1304/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    /*
+     Given an integer n, return any array containing n unique integers such that they add up to 0.
+     */
+
+    static void Main(string[] args)
+    {
+
+    }
+    public int[] SumZero(int n)
+    {
+        //-2,-1,0,1,2
+        //n=6
+        // -3,-2,-1,1,2,3
+        int[] result = new int[n];
+        if(n % 2 ==0)
+        {
+            for(int i = 0; i<n/2; i++)
+            {
+                result[i]=i-n/2;
+            }
+            for(int j=0;j<n/2; j++)
+            {
+                result[j+n/2]=j+1;
+            }
+        }
+        else
+        {
+            result[n/2]=0;
+            for(int i = 0; i<n/2; i++)
+            {
+                result[i]=i-n/2;
+            }
+            for(int j=0;j<n/2; j++)
+            {
+                result[j+n/2+1]=j+1;
+            }
+        }
+        return result;
+    }
+}
---
+++ 3151/3151/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    /*
+     An array is considered special if every pair of its adjacent elements contains two numbers with different parity.
+
+    You are given an array of integers nums. Return true if nums is a special array, otherwise, return false.
+     */
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsArraySpecial(new int[] {2,1,4}));
+    }
+    public static bool IsArraySpecial(int[] nums)
+    {
+        //2,1,4
+        for(int i = 0; i<nums.Length-1; i++)
+        {
+            if (nums[i]%2 == nums[i+1]%2)          
+                return false;  
+        }
+        return true;
+    }
+}
