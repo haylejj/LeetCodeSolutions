009810b3be323904455b7293764827f1a03f2b5c
cagataycingiz <cagatayberk01@gmail.com>
2024-07-02 01:37:18 +0300

    2089,905 easy

---
+++ 2089/2089/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    /*
+     You are given a 0-indexed integer array nums and a target element target.
+
+    A target index is an index i such that nums[i] == target.
+
+    Return a list of the target indices of nums after sorting nums in non-decreasing order. 
+    If there are no target indices, return an empty list. The returned list must be sorted in increasing order.
+     */
+
+    static void Main(string[] args)
+    {
+        
+    }
+    public IList<int> TargetIndices(int[] nums, int target)
+    {
+        Array.Sort(nums);
+        List<int> result = new List<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target)
+            {
+                result.Add(i);
+            }
+            if(nums[i] > target)
+            {
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ 905/905/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    /*
+     Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+
+    Return any array that satisfies this condition.
+     */
+
+    static void Main(string[] args)
+    {
+
+    }
+    public int[] SortArrayByParity(int[] nums)
+    {
+        int k= 0;
+        for(int i = 0; i<nums.Length; i++)
+        {
+            if (nums[i]%2==0)
+            {
+                int temp=nums[i];
+                nums[i]=nums[k];
+                nums[k]=temp;
+                k++;
+            }
+        }
+        return nums;
+    }
+}
