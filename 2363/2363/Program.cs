aa4c092f3f3b7e7c2a5a07594defc0c1ce624fe4
cagataycingiz <cagatayberk01@gmail.com>
2024-07-02 15:36:31 +0300

    2363,2965 easy

---
+++ 2363/2363/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+class Program
+{
+    /*
+     You are given two 2D integer arrays, items1 and items2, representing two sets of items. Each array items has the following properties:
+
+    items[i] = [valuei, weighti] where valuei represents the value and weighti represents the weight of the ith item.
+    The value of each item in items is unique.
+    Return a 2D integer array ret where ret[i] = [valuei, weighti], with weighti being the sum of weights of all items with value valuei.
+
+    Note: ret should be returned in ascending order by value.
+
+ 
+     */
+    static void Main(string[] args)
+    {
+
+    }
+    public IList<IList<int>> MergeSimilarItems(int[][] items1, int[][] items2)
+    {
+        /*
+        items1 =
+        [[1,3],[2,2]]
+        items2 =
+        [[7,1],[2,2],[1,4]]
+         */
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        foreach (var item in items1)
+        {
+            if (dict.ContainsKey(item[0])) dict[item[0]]+=item[1];
+            else dict.Add(item[0], item[1]);
+        }
+        foreach (var item in items2)
+        {
+            if (dict.ContainsKey(item[0])) dict[item[0]]+=item[1];
+            else dict.Add(item[0], item[1]);
+        }
+        List<IList<int>> result = new List<IList<int>>();
+        foreach (var item in dict)
+        {
+            result.Add(new List<int> { item.Key, item.Value });
+        }
+        result.Sort((a, b) => a[0].CompareTo(b[0]));
+        return result;
+    }
+}
---
+++ 2965/2965/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    /*
+     You are given a 0-indexed 2D integer matrix grid of size n * n with values in the range [1, n2]. 
+     Each integer appears exactly once except a which appears twice and b which is missing. 
+    The task is to find the repeating and missing numbers a and b.
+    Return a 0-indexed integer array ans of size 2 where ans[0] equals to a and ans[1] equals to b.
+     */
+    static void Main(string[] args)
+    {
+
+    }
+    public int[] FindMissingAndRepeatedValues(int[][] grid)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        int n = grid.Length;
+        int i = 0;
+        foreach (var item in grid)
+        {
+            foreach (var col in item)
+            {
+                if (dict.ContainsKey(col)) dict[col]++;
+                else dict[col]=1;
+            }
+        }
+        int[] result = new int[2];
+        foreach(var item in dict)
+            if(item.Value==2) result[0] = item.Key;
+        
+        for (i = 1; i <= n * n; i++)
+        {
+            if(!dict.ContainsKey(i))
+            {
+                result[1] = i;
+                break;
+            }
+        }
+        return result;
+    }
+}
