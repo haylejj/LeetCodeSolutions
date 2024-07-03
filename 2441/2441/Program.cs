726fac31a1194875d70d6cbe082daafbde4468a3
cagataycingiz <cagatayberk01@gmail.com>
2024-07-03 23:52:29 +0300

    2441,2848 easy

---
+++ 2441/2441/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    /*
+     Given an integer array nums that does not contain any zeros, find the largest positive integer k such that -k also exists in the array.
+
+    Return the positive integer k. If there is no such integer, return -1.
+
+ 
+     */
+    static void Main(string[] args)
+    {
+
+    }
+    public int FindMaxK(int[] nums)
+    {
+        HashSet<int> set = new HashSet<int>(nums);
+        int max = -1;
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(set.Contains(-nums[i]))
+            {
+                max= Math.Max(max, nums[i]);
+            }
+        }
+        return max;
+    }
+}
---
+++ 2848/2848/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    /*
+     You are given a 0-indexed 2D integer array nums representing the coordinates of the cars parking on a number line. 
+     For any index i, nums[i] = [starti, endi] where starti is the starting point of the ith car and endi is the ending point of the ith car.
+     Return the number of integer points on the line that are covered with any part of a car.
+     */
+    static void Main(string[] args)
+    {
+
+    }
+    public int NumberOfPoints(IList<IList<int>> nums)
+    {
+        HashSet<int> set = new HashSet<int>();
+
+        foreach (var item in nums)
+        {
+            var startPoint = item[0];
+            var endPoint = item[1];
+
+            for (int i = startPoint; i <= endPoint; i++)
+                set.Add(i);
+        }
+
+        return set.Count;
+    }
+}
