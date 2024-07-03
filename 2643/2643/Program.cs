d81aad1f95f08e90db362b5eabb45e89396df7b7
cagataycingiz <cagatayberk01@gmail.com>
2024-07-03 22:46:12 +0300

    2643,2951 easy

---
+++ 2643/2643/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    /*
+     Given a m x n binary matrix mat, find the 0-indexed position of the row that contains the maximum count of ones, and the number of ones in that row.
+
+    In case there are multiple rows that have the maximum count of ones, the row with the smallest row number should be selected.
+
+    Return an array containing the index of the row, and the number of ones in it.
+     */
+    static void Main(string[] args)
+    {
+        Console.WriteLine(RowAndMaximumOnes(new int[][] { new int[] {0,1 }, new int[] { 1,0 } }));
+    }
+    public static  int[] RowAndMaximumOnes(int[][] mat)
+    {
+        //[0,1],[1,0]
+        //0,1
+        //1,0
+        int[] result= new int[2];
+        int max = 0;
+        for(int i = 0; i<mat.Length; i++)
+        {
+            var count= mat[i].Count(x=>x==1);
+            if (count>max )
+            {
+                result[0]=i;
+                result[1]=count;
+                max = count;
+            }
+        }
+        return result;
+    }
+}
---
+++ 2951/2951/Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    /*
+     You are given a 0-indexed array mountain. Your task is to find all the peaks in the mountain array.
+
+    Return an array that consists of indices of peaks in the given array in any order.
+
+    Notes:
+
+    A peak is defined as an element that is strictly greater than its neighboring elements.
+    The first and last elements of the array are not a peak.
+     */
+    static void Main(string[] args)
+    {
+
+    }
+    public IList<int> FindPeaks(int[] mountain)
+    {
+        //1,4,3,8,5
+        List<int> result = new List<int>();
+        for(int i=1;i<mountain.Length-1;i++)
+        {
+            if(mountain[i]>mountain[i-1] && mountain[i]>mountain[i+1])
+            {
+                result.Add(i);
+            }
+        }
+        return result;
+    }
+}
