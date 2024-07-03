ae875f3e5bab3a8201b6be228d91116341878e8b
cagataycingiz <cagatayberk01@gmail.com>
2024-07-03 23:10:31 +0300

    2032,2341 easy

---
+++ 2032/2032/Program.cs
@@ -0,0 +1,49 @@
+﻿class Program
+{
+    /*
+     Given three integer arrays nums1, nums2, and nums3, 
+     return a distinct array containing all the values that are present in at least two out of the three arrays. You may return the values in any order.
+     */
+    static void Main(string[] args)
+    {
+
+    }
+    public IList<int> TwoOutOfThree(int[] nums1, int[] nums2, int[] nums3)
+    {
+        HashSet<int> dict = new HashSet<int>();
+        foreach (var item in nums1)
+        {
+            dict.Add(item);
+        }
+        foreach (var item in nums2)
+        {
+            dict.Add(item);
+        }
+        foreach (var item in nums3)
+        {
+            dict.Add(item);
+        }
+        List<int> result = new List<int>();
+        foreach (var item in dict)
+        {
+            int count = 0;
+            if (nums1.Contains(item))
+            {
+                count++;
+            }
+            if (nums2.Contains(item))
+            {
+                count++;
+            }
+            if (nums3.Contains(item))
+            {
+                count++;
+            }
+            if (count >= 2)
+            {
+                result.Add(item);
+            }
+        }
+        return result;
+    }
+}
---
+++ 2341/2341/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    /*
+     You are given a 0-indexed integer array nums. In one operation, you may do the following:
+    Choose two integers in nums that are equal.
+    Remove both integers from nums, forming a pair.
+    The operation is done on nums as many times as possible.
+    Return a 0-indexed integer array answer of size 2 where answer[0] is the number of pairs that are formed and answer[1] is the number of leftover integers in nums after doing the operation as many times as possible.
+     */
+    static void Main(string[] args)
+    {
+
+    }
+    public int[] NumberOfPairs(int[] nums)
+    {
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+        foreach (var item in nums)
+        {
+            if (dic.ContainsKey(item))
+            {
+                dic[item]++;
+            }
+            else
+            {
+                dic.Add(item, 1);
+            }
+        }
+        int pairs = 0;
+        int left = 0;
+        foreach (var item in dic)
+        {
+            pairs += item.Value / 2;
+            left += item.Value % 2;
+        }
+        return new int[] { pairs, left };
+    }
+}
