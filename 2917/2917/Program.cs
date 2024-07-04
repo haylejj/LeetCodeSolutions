c31c3bef4770cc48c6bb2e4ada13fa6a568f1ec8
cagataycingiz <cagatayberk01@gmail.com>
2024-07-04 21:26:01 +0300

    2917,944 easy

---
+++ 2917/2917/Program.cs
@@ -0,0 +1,53 @@
+﻿class Program
+{
+    /*
+     You are given an integer array nums, and an integer k. Let's introduce K-or operation by extending the standard bitwise OR. In K-or, a bit position in the result is set to 1 if at least k numbers in nums have a 1 in that position.
+
+    Return the K-or of nums.
+     */
+    static void Main(string[] args)
+    {
+
+    }
+    public int FindKOr(int[] nums, int k)
+    {
+        int[][] arr = new int[nums.Length][];
+        //binary koda çevirme işlemi Convert.ToString(num, 2); ile de yapılabilir.
+        for (int i = 0; i < nums.Length; i++)
+        {
+            arr[i] = new int[32];
+            int num = nums[i];
+            for (int j = 31; j >= 0; j--)
+            {
+                arr[i][j] = num & 1;
+                num >>= 1;
+            }
+        }
+
+        for (int l = 31; l >= 0; l--)
+        {
+            int count = 0;
+            for (int m = 0; m < arr.Length; m++)
+            {
+                if (arr[m][l] == 1)
+                    count++;
+            }
+
+            if (count >= k)
+            {
+                for (int n = 0; n < arr.Length; n++)
+                    arr[n][l] = 1;
+            }
+            else
+            {
+                for (int n = 0; n < arr.Length; n++)
+                    arr[n][l] = 0;
+            }
+        }
+
+        string resultBinary = string.Join("", arr[0]);
+        int result = Convert.ToInt32(resultBinary,2);
+
+        return result;
+    }
+}
---
+++ 944/944/Program.cs
@@ -0,0 +1,36 @@
+﻿class Program
+{
+    /*
+     You are given an array of n strings strs, all of the same length.
+
+    The strings can be arranged such that there is one on each line, making a grid.
+
+    For example, strs = ["abc", "bce", "cae"] can be arranged as follows:
+    abc
+    bce
+    cae
+    You want to delete the columns that are not sorted lexicographically. In the above example (0-indexed), columns 0 ('a', 'b', 'c') and 2 ('c', 'e', 'e') are sorted, while column 1 ('b', 'c', 'a') is not, so you would delete column 1.
+
+    Return the number of columns that you will delete.
+     */
+    static void Main(string[] args)
+    {
+        Console.WriteLine(MinDeletionSize(new string[] { "cba", "daf", "ghi" }));
+    }
+    public static int MinDeletionSize(string[] strs)
+    {
+        int count = 0;
+        for (int i = 0; i<strs[0].Length; i++)
+        {
+            for (int j = 0; j<strs.Length-1; j++)
+            {
+                if (strs[j+1][i] < strs[j][i])
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
