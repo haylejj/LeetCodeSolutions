a4197bcd95d0d8afe7472a21fbbf4200290a4ea2
cagataycingiz <cagatayberk01@gmail.com>
2024-07-04 17:46:15 +0300

    1122 was tough. 118 easy

---
+++ 1122/1122/Program.cs
@@ -0,0 +1,59 @@
+﻿class Program
+{
+    /*
+     Given two arrays arr1 and arr2, the elements of arr2 are distinct, and all elements in arr2 are also in arr1.
+
+    Sort the elements of arr1 such that the relative ordering of items in arr1 are the same as in arr2. 
+    Elements that do not appear in arr2 should be placed at the end of arr1 in ascending order.
+     */
+    static void Main(string[] args)
+    {
+        Console.WriteLine(RelativeSortArray(new int[] { 2, 21, 43, 38, 0, 42, 33, 7, 24, 13, 12, 27, 12, 24, 5, 23, 29, 48, 30, 31 },new int[] { 2, 42, 38, 0, 43, 21 }));
+    }
+    public static int[] RelativeSortArray(int[] arr1, int[] arr2)
+    {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+
+        foreach (var item in arr1)
+        {
+            if (dict.ContainsKey(item))
+            {
+                dict[item]++;
+            }
+            else
+            {
+                dict[item] = 1;
+            }
+        }
+
+        List<int> result = new List<int>();
+
+        foreach (var item in arr2)
+        {
+            if (dict.ContainsKey(item))
+            {
+                int count = dict[item];
+                while (count > 0)
+                {
+                    result.Add(item);
+                    count--;
+                }
+                dict.Remove(item);
+            }
+        }
+
+        List<int> remaining = new List<int>(dict.Keys);
+        remaining.Sort();
+        foreach (var item in remaining)
+        {
+            int count = dict[item];
+            while (count > 0)
+            {
+                result.Add(item);
+                count--;
+            }
+        }
+
+        return result.ToArray();
+    }
+}
---
+++ 118/118/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    /*
+     Given an integer numRows, return the first numRows of Pascal's triangle.
+
+    In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+    1                                      
+    1   1   
+    1   2   1
+    1   3   3   1
+    1   4   6   4   1
+     */
+    static void Main(string[] args)
+    {
+
+    }
+    public IList<IList<int>> Generate(int numRows)
+    {
+        int [][] result = new int[numRows][];
+        for(int i = 0; i<numRows; i++)
+        {
+            result[i]=new int[i+1];
+            result[i][0]=1;
+            result[i][i]=1;
+            for(int k = 1; k<i; k++)
+            {
+                result[i][k]=result[i-1][k-1]+result[i-1][k];
+            }
+        }
+        return result;
+    }
+}
