503c85be40b5b744ce6fd1bab5e4df957e4583df
cagataycingiz <cagatayberk01@gmail.com>
2024-07-05 13:01:10 +0300

    1002,2586 easy

---
+++ 1002/1002/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+
+class Program
+{
+    /*
+     Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). 
+     You may return the answer in any order
+     */
+    static void Main(string[] args)
+    {
+        Console.WriteLine(CommonChars(new string[] { "bella", "label", "roller" }));
+    }
+    public static IList<string> CommonChars(string[] words)
+    {
+        //"bella","label","roller"
+        List<string> result = new List<string>();
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        foreach (var c in words[0])
+        {
+            if (dict.ContainsKey(c))
+            {
+                dict[c]++;
+            }
+            else
+            {
+                dict.Add(c, 1);
+            }
+        }
+        for (int i = 1; i < words.Length; i++)
+        {
+            Dictionary<char, int> temp = new Dictionary<char, int>();
+            foreach (var c in words[i])
+            {
+                if (temp.ContainsKey(c))
+                {
+                    temp[c]++;
+                }
+                else
+                {
+                    temp.Add(c, 1);
+                }
+            }
+            foreach (var item in dict)
+            {
+                if (temp.ContainsKey(item.Key))
+                {
+                    dict[item.Key] = Math.Min(dict[item.Key], temp[item.Key]);
+                }
+                else
+                {
+                    dict[item.Key] = 0;
+                }
+            }
+        }
+        foreach (var item in dict)
+        {
+            for (int i = 0; i < item.Value; i++)
+            {
+                result.Add(item.Key.ToString());
+            }
+        }
+        return result;
+
+    }
+}
---
+++ 2586/2586/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    /*
+     You are given a 0-indexed array of string words and two integers left and right.
+
+    A string is called a vowel string if it starts with a vowel character and ends with a vowel character where vowel characters are 'a', 'e', 'i', 'o', and 'u'.
+
+    Return the number of vowel strings words[i] where i belongs to the inclusive range [left, right].
+     */
+    static void Main(string[] args)
+    {
+
+    }
+    public int VowelStrings(string[] words, int left, int right)
+    {
+        int count = 0;
+        string vowel= "aeiou";
+        for (int i = left; i<=right; i++)
+        {
+            if (vowel.Contains(words[i][0]) && vowel.Contains(words[i][words[i].Length-1])) count++;
+        }
+        return count;
+    }
+}
