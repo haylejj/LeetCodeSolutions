9c8f223a7140820b58b239459d1c1d4211137428
cagataycingiz <cagatayberk01@gmail.com>
2024-07-06 13:19:20 +0300

    136,2053,2923

---
+++ 2053/2053/Program.cs
@@ -0,0 +1,43 @@
+﻿class Program
+{
+    /*
+     A distinct string is a string that is present only once in an array.
+
+    Given an array of strings arr, and an integer k, return the kth distinct string present in arr. If there are fewer than k distinct strings, return an empty string "".
+
+    Note that the strings are considered in the order in which they appear in the array.
+     */
+    static void Main(string[] args)
+    {
+        Console.WriteLine(KthDistinct(new string[] { "a", "b", "a", "c" }, 2));
+    }
+    public static string KthDistinct(string[] arr, int k)
+    {
+        //LinkedList<string> set = new LinkedList<string>();
+        //List<string> list = new List<string>();
+        //foreach(var item in arr)
+        //{
+        //    if(!set.Contains(item) && !list.Contains(item)) set.AddLast(item);
+        //    else
+        //    {
+        //        set.Remove(item);
+        //        list.Add(item);
+        //    }
+        //}
+        //if(set.Count < k) return "";
+        //return set.ElementAt(k-1);
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+        foreach (var item in arr)
+        {
+            if (dict.ContainsKey(item)) dict[item]++;
+            else dict.Add(item, 1);
+        }
+        int dictinctCount = 0;
+        foreach(var item in dict)
+        {
+            if (item.Value == 1) dictinctCount++;
+            if (dictinctCount == k) return item.Key;
+        }
+        return "";
+    }
+}
---
+++ 2923/2923/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    /*
+    There are n teams numbered from 0 to n - 1 in a tournament.
+    Given a 0-indexed 2D boolean matrix grid of size n * n. 
+    For all i, j that 0 <= i, j <= n - 1 and i != j team i is stronger than team j if grid[i][j] == 1, otherwise, team j is stronger than team i.
+    Team a will be the champion of the tournament if there is no team b that is stronger than team a.
+    Return the team that will be the champion of the tournament.
+     */
+    static void Main(string[] args)
+    {
+        Console.WriteLine(FindChampion(new int[][] {new int[] { 0, 0, 1 }, new int[] { 1, 0, 1 }, new int[] { 0, 0, 0 } }));
+    }
+    public static int FindChampion(int[][] grid)
+    {
+        int n = grid.Length;
+        int count = 0;
+        int winner=0;
+        for(int i = 0; i<n; i++)
+        {
+            for(int j = 0; j<n; j++)
+            {
+                if(i==j) continue;
+                if (grid[i][j]==1) count++;
+            }
+            if(count==n-1) winner= i;
+            count = 0;
+        }
+        return winner;
+    }
+}
