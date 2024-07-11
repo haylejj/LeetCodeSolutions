f0ba940a937bfc5a80984c45d436596da46a56fb
cagataycingiz <cagatayberk01@gmail.com>
2024-07-11 21:38:07 +0300

    766,999

---
+++ 766/766/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+    public bool IsToeplitzMatrix(int[][] matrix)
+    {
+        bool result = false;
+        for (int i = 1; i<matrix.Length; i++)
+        {
+            for(int j = 1; j<matrix[i].Length; j++)
+            {
+                if (matrix[i][j] != matrix[i-1][j-1]) return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ 999/999/Program.cs
@@ -0,0 +1,65 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+    public int NumRookCaptures(char[][] board)
+    {
+        //'R', '.', 'B', or 'p' R=kale B=fil p=piyon .=boş
+        int x = 0, y = 0;
+        for(int i = 0; i<8; i++)
+        {
+            for(int j = 0; j<8; j++)
+            {
+                if (board[i][j]=='R')
+                {
+                    x= i;
+                    y = j;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = y; i >= 0; i--)
+        {
+            if (board[x][i] == 'B')
+                break;
+            if (board[x][i] == 'p')
+            {
+                count++;
+                break;
+            }
+        }
+        for (int i = y; i < 8; i++)
+        {
+            if (board[x][i] == 'B')
+                break;
+            if (board[x][i] == 'p')
+            {
+                count++;
+                break;
+            }
+        }
+        for (int i = x; i >= 0; i--)
+        {
+            if (board[i][y] == 'B')
+                break;
+            if (board[i][y] == 'p')
+            {
+                count++;
+                break;
+            }
+        }
+        for (int i = x; i < 8; i++)
+        {
+            if (board[i][y] == 'B')
+                break;
+            if (board[i][y] == 'p')
+            {
+                count++;
+                break;
+            }
+        }
+        return count;
+    }
+}
