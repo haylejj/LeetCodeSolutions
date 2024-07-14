06c9687b884147bdceb3354fc51f342ba05208e9
cagataycingiz <cagatayberk01@gmail.com>
2024-07-14 15:18:04 +0300

    929 easy

---
+++ 929/929/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(NumUniqueEmails(new string[] { "test.email+alex@leetcode.com", "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com" }));
+    }
+    public static int NumUniqueEmails(string[] emails)
+    {
+        HashSet<string> set = new HashSet<string>();
+        for(int i = 0; i<emails.Length; i++)
+        {
+            var email = emails[i].Split('@');
+            var localName= email[0];
+            var domainName= email[1];
+            if (localName.Contains('+'))
+                localName=localName.Split('+')[0];
+            if (localName.Contains('.')) localName=localName.Replace(".", string.Empty);
+            if(!set.Contains(localName+"@"+domainName)) set.Add(localName+"@"+domainName);
+        }
+        return set.Count;
+    }
+}
