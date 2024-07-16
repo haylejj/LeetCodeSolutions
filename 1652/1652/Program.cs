f8a77bad86d329a9c5eb653df468014dc5a8831c
cagataycingiz <cagatayberk01@gmail.com>
2024-07-16 18:19:58 +0300

    1652,2899 easy

---
+++ 1652/1652/Program.cs
@@ -0,0 +1,52 @@
+﻿class Program
+{
+    /*
+     * You have a bomb to defuse, and your time is running out! Your informer will provide you with a circular array code of length of n and a key k.
+
+To decrypt the code, you must replace every number. All the numbers are replaced simultaneously.
+
+If k > 0, replace the ith number with the sum of the next k numbers.
+If k < 0, replace the ith number with the sum of the previous k numbers.
+If k == 0, replace the ith number with 0.
+As code is circular, the next element of code[n-1] is code[0], and the previous element of code[0] is code[n-1].
+
+Given the circular array code and an integer key k, return the decrypted code to defuse the bomb!
+     */
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Decrypt(new int[] { 5, 7, 1, 4 },-3));
+    }
+    public static int[] Decrypt(int[] code, int k)
+    {
+        int n = code.Length;
+        int[] result = new int[n];
+        if (k == 0)
+        {
+            Array.Fill(code, 0);
+            return code;
+        }
+        else if (k>0)
+        {
+            for (int i = 0; i<n; i++)
+            {
+                var tempSum = 0;
+                for (int j = 0; j<k; j++)
+                    tempSum+=code[(i+j+1)%n];
+                result[i]=tempSum;
+            }
+            return result;
+        }
+        else
+        {
+            for (int i = 0; i<n; i++)
+            {
+                var tempSum = 0;
+                for (int j = 1; j<=-k; j++)
+                    tempSum+=code[(i-j+n)%n];
+                result[i]=tempSum;
+            }
+            return result;
+        }
+
+    }
+}
---
+++ 2899/2899/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        LastVisitedIntegers(new int[] { -1, -1, 94, 56, -1, 32, -1, -1, -1 });
+    }
+    public static IList<int> LastVisitedIntegers(int[] nums)
+    {
+        List<int> seen = new List<int>();
+        List<int> ans= new List<int>();
+        int countMinusOne= 0;
+        for(int i = 0; i<nums.Length; i++)
+        {
+            if (nums[i]>0) { seen.Insert(0,nums[i]);countMinusOne=0; }
+            else
+            {
+                countMinusOne++;
+                if (countMinusOne<=seen.Count) ans.Add(seen[countMinusOne-1]);
+                else ans.Add(-1);
+            }
+
+        }
+        return ans;
+    }
+}
